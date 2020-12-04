b6336a1437d2bd8b52b50a37a6a78ff0b590cab1
2021-DEV1-002@bnp.org <alixamou1@gmail.com>
2020-12-04 15:02:24 +0100

    Unit Tests
    Add of unit tests for TDD

--- src/test/java/bnp/kata/java/tictactoe/TictactoeApplicationTests.java
+++ src/test/java/bnp/kata/java/tictactoe/TictactoeApplicationTests.java
@@ -2,12 +2,41 @@ package bnp.kata.java.tictactoe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.Assert;
 
 @SpringBootTest
 class TictactoeApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void isGridFilledOfXAnd0() {
+        // TODO Get the current grid
+        char[][] tab = {{'x', '0', 'x'}, {'x', '0', '0'}, {'x', '0', '0'}};
+        for (char[] line : tab) {
+            for (char character : line) {
+                Assert.isTrue(character == 'x' || character == '0', "Grid contains no handle character");
+            }
+        }
+    }
+
+    @Test
+    void isGridFull() {
+        char[][] tab = {{'x', '0', 'x'}, {'0', ' ', 'x'}, {'x', '0', ' '}};
+        // TODO Implement and call the service that check that
+        for (char[] line : tab) {
+            for (char character : line) {
+                    Assert.isTrue(character == 'x' || character == '0' || character == ' ', "Grid is not full");
+            }
+        }
+    }
+
+    @Test
+	void playerWin()
+	{
+		char[][] tab = {{'x', '0', ' '}, {'x', ' ', '0'}, {'x', '0', 'x'}};
+		// TODO Implement and call the service that check that
+	}
 }
